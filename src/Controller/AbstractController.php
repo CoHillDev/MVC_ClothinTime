6219da64b95ef60eda19cde8a259e4190c446d6a
CoHillDev <[colinemv.dev@gmail.com](mailto:colinemv.dev@gmail.com)>
2023-04-18 12:46:39 +0200

    Login/Register - MVC

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -9,12 +10,14 @@ use Twig\Loader\FilesystemLoader;
 abstract class AbstractController
 {
     protected Environment $twig;
-
+    protected array|false $user;
     /**
      *  Initializes this class.
      */
+
     public function __construct()
     {
+        // Initialisation de l'objet Twig
         $loader = new FilesystemLoader(APP_VIEW_PATH);
         $this->twig = new Environment(
             $loader,
@@ -23,6 +26,15 @@ abstract class AbstractController
                 'debug' => (ENV === 'dev'),
             ]
         );
+
         $this->twig->addExtension(new DebugExtension());
+
+        // Initialisation de la propriété $user
+        $userManager = new UserManager();
+        // $this->user = isset($_SESSION['email']) ? $userManager->selectOneByEmail($_SESSION['email']) : false;
+        $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
+
+        // Ajout de $user aux variables globales de Twig
+        $this->twig->addGlobal('user', $this->user);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,93 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
+
 class UserController extends AbstractController
 {
+
     public function login(): string
     {
-        return $this->twig->render('User/login.html.twig');
+        $credentials = array_map('trim', $_POST);
+
+        //@todo make some controls on email and password fields and if errors, send them to the view :
+
+        //@todo add controls for email and password fields
+        $errors = [];
+        if (empty($credentials['email'])) {
+            $errors[] = 'Email is required';
+        } elseif (!filter_var($credentials['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Invalid email format';
+        }
+
+        if (empty($credentials['password'])) {
+            $errors[] = 'Password is required';
+        }
+
+        if (!empty($errors)) {
+            return $this->twig->render('User/login.html.twig', ['errors' => $errors]);
+        }
+
+        //@todo check if the user exists in the database
+        $userManager = new UserManager();
+        $user = $userManager->selectOneByEmail($credentials['email']);
+        if ($user && password_verify($credentials['password'], $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            header('Location: /');
+            exit();
+        }
+
+        $errors[] = 'Incorrect email or password';
+        return $this->twig->render('User/login.html.twig', ['errors' => $errors]);
     }
 
     public function logout()
     {
+        //session_start(); // on démarre la session - déjà on
+        unset($_SESSION['user_id']); // on détruit la variable de session "user_id"
+        session_destroy(); // on détruit la session
+
+        // on redirige l'utilisateur vers la page d'accueil
+        header('Location: index.php');
+        exit;
     }
 
-    public function register(): string
+    public function register()
     {
-        return $this->twig->render('User/register.html.twig');
+        $errors = [];
+        // @todo make some controls and if errors send them to the view :
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Get the form data
+            $credentials = $_POST;
+
+            // Validate the form data
+            if (empty($credentials['email'])) {
+                $errors[] = 'Email is required';
+            }
+            if (empty($credentials['password'])) {
+                $errors[] = 'Password is required';
+            }
+            if (empty($credentials['pseudo'])) {
+                $errors[] = 'Pseudo is required';
+            }
+            if (empty($credentials['firstname'])) {
+                $errors[] = 'Firstname is required';
+            }
+            if (empty($credentials['lastname'])) {
+                $errors[] = 'Lastname is required';
+            }
+
+            // If there are no errors, insert the user
+            if (empty($errors)) {
+                $userManager = new UserManager();
+                if ($userManager->insert($credentials)) {
+                    return $this->login();
+                }
+            }
+        }
+
+        return $this->twig->render('User/register.html.twig', [
+            'errors' => $errors
+        ]);
     }
 }
